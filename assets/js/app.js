795ee19f53d8f21c6782e7e93fa4a67211cd1194
Naim Hossain <najmulspark@gmail.com>
2022-07-04 21:22:43 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+
+  'use strict';
+
+  // =====================
+  // Off Canvas Menu
+  // =====================
+
+  $('.js-off-canvas-toggle').click(function(e) {
+    e.preventDefault();
+    $('.js-off-canvas-content, .js-off-canvas-container').toggleClass('is-active');
+  });
+
+  // =====================
+  // Homepage Layout
+  // =====================
+
+  // Make the second and third posts in homepage be 50% width
+  $('.home-template .js-post-card-wrap:nth-of-type(2), .home-template .js-post-card-wrap:nth-of-type(3)')
+  .addClass('o-grid__col--2-4-m o-grid__col--2-4-l');
+
+  // =====================
+  // Post Card Images Fade
+  // =====================
+
+  $('.js-fadein').viewportChecker({
+    classToAdd: 'is-inview', // Class to add to the elements when they are visible
+    offset: 100,
+    removeClassAfterAnimation: true
+  });
+
+  // =====================
+  // Koenig Gallery
+  // =====================
+  var gallery_images = document.querySelectorAll('.kg-gallery-image img');
+
+  gallery_images.forEach(function (image) {
+    var container = image.closest('.kg-gallery-image');
+    var width = image.attributes.width.value;
+    var height = image.attributes.height.value;
+    var ratio = width / height;
+    container.style.flex = ratio + ' 1 0%';
+  });
+
+  // =====================
+  // Responsive Videos
+  // =====================
+
+  $('.c-content').fitVids({
+    'customSelector': [ 'iframe[src*="ted.com"]'          ,
+                        'iframe[src*="player.twitch.tv"]' ,
+                        'iframe[src*="dailymotion.com"]'  ,
+                        'iframe[src*="facebook.com"]'
+                      ]
+  });
+
+  // =====================
+  // Search
+  // =====================
+
+  var search_field = $('.js-search-input'),
+      search_results = $('.js-search-results'),
+      toggle_search = $('.js-search-toggle'),
+      search_result_template = "\
+      <a href={{link}} class='c-search-result'>\
+        <div class='c-search-result__content'>\
+          <h3 class='c-search-result__title'>{{title}}</h3>\
+          <time class='c-search-result__date'>{{pubDate}}</time>\
+        </div>\
+        <div class='c-search-result__media'>\
+          <div class='c-search-result__image is-inview' style='background-image: url({{featureImage}})'></div>\
+        </div>\
+      </a>";
+
+  toggle_search.click(function(e) {
+    e.preventDefault();
+    $('.js-search').addClass('is-active');
+
+    // If off-canvas is active, just disable it
+    $('.js-off-canvas-container').removeClass('is-active');
+
+    setTimeout(function() {
+      search_field.focus();
+    }, 500);
+  });
+
+  $('.c-search, .js-search-close, .js-search-close .icon').on('click keyup', function(event) {
+    if (event.target == this || event.target.className == 'js-search-close' || event.target.className == 'icon' || event.keyCode == 27) {
+      $('.c-search').removeClass('is-active');
+    }
+  });
+
+  search_field.ghostHunter({
+    results: search_results,
+    onKeyUp         : true,
+    result_template : search_result_template,
+    zeroResultsInfo : false,
+    displaySearchInfo: false,
+    before: function() {
+      search_results.fadeIn();
+    }
+  });
+});
